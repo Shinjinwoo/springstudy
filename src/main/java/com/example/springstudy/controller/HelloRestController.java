f21b283a9c2a3dcff5b4a3d0f4e9bb160a65742a
shin-jinwoo <jinu712@gmail.com>
2022-07-31 13:56:05 +0900

    templates 추가

--- src/main/java/com/example/springstudy/controller/HelloRestController.java
+++ src/main/java/com/example/springstudy/controller/HelloRestController.java
@@ -6,9 +6,11 @@ import org.mariadb.jdbc.internal.logging.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -25,14 +27,26 @@ public class HelloRestController {
     }
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model) {
 
         System.out.println("test22");
+        String msg = "민지야 안녕 하루종일 코딩 한다고 섭섭했지 하루종일 서버 판다고 바빳엉 ㅎㅎㅎ" +
+                " 우리 앞으로 더 행복하자 우리 민지 캬햐아에서 급체 했다고" +
+                " 들었는데 아프지말아용 !";
+
+        model.addAttribute("data",msg);
 
         logger.debug(":::::::::::::::::::::::::::");
-        return "민지야 안녕 하루종일 코딩 한다고 섭섭했지 \n" +
-                "하루종일 서버 판다고 바빳엉 ㅎㅎㅎ 우리 앞으로 더 행복하자 \n" +
-                "우리 민지 캬햐아에서 급체 했다고 들었는데 아프지말아용 !";
+
+        return msg;
 
     };
+
+    /** @ReqeustParam **/
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("data",name);
+        return "hello-mvc";
+    }
+
 }
